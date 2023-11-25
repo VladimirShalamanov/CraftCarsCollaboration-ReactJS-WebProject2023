169c85c0312a0e3d7130b16d27d030e6dd004c45
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-25 18:19:10 +0200

    Add context for authorization and paths

---
+++ client/src/contexts/authContext.js
@@ -0,0 +1,69 @@
+import { createContext } from "react";
+import { useNavigate } from 'react-router-dom';
+
+import * as authService from '../services/authService';
+import usePersistedState from "../hooks/usePersistedState";
+import Path from "../utils/paths";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({
+    children,
+}) => {
+    const navigate = useNavigate();
+    const [auth, setAuth] = usePersistedState('auth', {});
+
+    const loginSubmitHandler = async (values) => {
+        const result = await authService.login(values.email, values.password);
+
+        setAuth(result);
+
+        localStorage.setItem('accessToken', result.accessToken);
+
+        navigate(Path.Home);
+    };
+
+    const registerSubmitHandler = async (values) => {
+        if (values.password !== values['confirm-password']) {
+            alert('Passwords do not match!');
+
+            navigate(Path.Register);
+        } else {
+
+            const result = await authService.register(values.email, values.password);
+
+            setAuth(result);
+
+            localStorage.setItem('accessToken', result.accessToken);
+
+            navigate(Path.Home);
+        }
+    };
+
+    const logoutHandler = async () => {
+        setAuth({});
+
+        localStorage.removeItem('accessToken');
+    };
+
+    const values = {
+        loginSubmitHandler,
+        registerSubmitHandler,
+        logoutHandler,
+        username: auth.username || auth.email,
+        email: auth.email,
+        userId: auth._id,
+        isAuthenticated: !!auth.accessToken,
+    };
+
+
+    return (
+        <AuthContext.Provider value={values}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthContext.displayName = 'AuthContext';
+
+export default AuthContext;
---
+++ client/src/utils/paths.js
@@ -0,0 +1,15 @@
+const Path = {
+    Home: '/',
+    Header: '/header',
+
+    // GameList: '/games',
+    // GameCreate: '/games/create',
+    // GameDetails: '/games/:gameId',
+    // GameEdit: '/games/:gameId/edit',
+    
+    Register: '/register',
+    Login: '/login',
+    Logout: '/logout',
+};
+
+export default Path;
