0bce5c6872f68a8799b0beac5f3ef45d1ba53134
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-26 14:56:01 +0200

    Simple example for home and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/authContext';
 import Path from './utils/paths';
 
-import Header from "./components/header/Header";
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import Footer from './components/footer/Footer';
+
 
 import Register from './components/register/Register';
 import Login from './components/login/Login';
@@ -13,14 +16,21 @@ export default function App() {
     return (
         <AuthProvider>
             <div id="child-box">
-                
+
                 <Header />
 
-                <Routes>
-                    <Route path={Path.Login} element={<Login />} />
-                    <Route path={Path.Logout} element={<Logout />} />
-                    <Route path={Path.Register} element={<Register />} />
-                </Routes>
+                <main>
+                    <Routes>
+                        <Route path={Path.Header} element={<Header />} />
+                        <Route path={Path.Home} element={<Home />} />
+
+                        <Route path={Path.Login} element={<Login />} />
+                        <Route path={Path.Logout} element={<Logout />} />
+                        <Route path={Path.Register} element={<Register />} />
+                    </Routes>
+                </main>
+
+                <Footer />
             </div>
         </AuthProvider>
     );
---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <section className="home-content">
+            <div className="-message">
+                <h2>Welcome To</h2>
+                <h3>The Car World</h3>
+                <h4>Personalize Your Dream Car</h4>
+            </div>
+        </section>
+    );
+};
