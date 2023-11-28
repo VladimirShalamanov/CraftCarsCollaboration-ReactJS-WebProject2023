6d9c91f9384854da4ee2f0a76faf9b5a3d086985
VladimirShalamanov <vladi.gesh@gmail.com>
2023-11-28 20:39:49 +0200

    Add Auth Guard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
 
 import { AuthProvider } from './contexts/authContext';
+import AuthGuard from './components/guards/AuthGuard';
+import ErrorBoundary from './components/error-boundary/ErrorBoundary';
 import Path from './utils/paths';
 
 import Header from './components/header/Header';
@@ -15,24 +17,29 @@ import Logout from './components/logout/Logout';
 
 export default function App() {
     return (
-        <AuthProvider>
-            <div id="child-box">
+        <ErrorBoundary>
+            <AuthProvider>
+                <div id="child-box">
 
-                <Header />
-                
-                <main>
-                    <Routes>
-                        <Route path={Path.Header} element={<Header />} />
-                        <Route path={Path.Home} element={<Home />} />
+                    <Header />
 
-                        <Route path={Path.Login} element={<Login />} />
-                        <Route path={Path.Logout} element={<Logout />} />
-                        <Route path={Path.Register} element={<Register />} />
-                    </Routes>
-                </main>
+                    <main>
+                        <Routes>
+                            <Route path={Path.Header} element={<Header />} />
+                            <Route path={Path.Home} element={<Home />} />
 
-                <Footer />
-            </div>
-        </AuthProvider>
+                            <Route path={Path.Login} element={<Login />} />
+                            <Route path={Path.Register} element={<Register />} />
+
+                            <Route element={<AuthGuard />} >
+                                <Route path={Path.Logout} element={<Logout />} />
+                            </Route>
+                        </Routes>
+                    </main>
+
+                    <Footer />
+                </div>
+            </AuthProvider>
+        </ErrorBoundary>
     );
 };
