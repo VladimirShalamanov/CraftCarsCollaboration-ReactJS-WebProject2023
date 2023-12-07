6af7c7869af402ce68ca6b8d31e0446126d1edbf
VladimirShalamanov <vladi.gesh@gmail.com>
2023-12-07 22:01:39 +0200

    Complete footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import "./footer.css";
 
 export default function Footer() {
-    return(
+    return (
         <footer>
-            <p>All rights reserved &copy;</p>
+            <section className="container">
+                <div className="component">
+                    <ul>
+                        <li>Newsroom</li>
+                        <li>Customers</li>
+                    </ul>
+                    <ul>
+                        <li>Carrers</li>
+                        <li>Where To Buy</li>
+                    </ul>
+                    <ul>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                    <ul>
+                        <li>Support</li>
+                        <li>Sponsors</li>
+                    </ul>
+                    <ul>
+                        <li>Forum</li>
+                        <li>Help Center</li>
+                    </ul>
+                </div>
+
+                <p>Made By Vladimir Shalamanov</p>
+                <p>All rights reserved &copy;</p>
+            </section>
         </footer>
     );
 };
