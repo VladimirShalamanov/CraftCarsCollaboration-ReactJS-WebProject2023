dad271c9e17b82fe3f3d8e68b60ede113ed1e253
VladimirShalamanov <vladi.gesh@gmail.com>
2023-12-08 14:04:55 +0200

    Complete car list item

--- client/src/components/car-list/car-list-item/CarListItem.jsx
+++ client/src/components/car-list/car-list-item/CarListItem.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
+import { pathToUrl } from "../../../utils/pathFix";
 import Path from "../../../utils/paths";
+
 import "./carListItem.css";
 
 export default function CarListItem({
@@ -15,9 +19,13 @@ export default function CarListItem({
 
             <div className="intro">
                 <h4>{make}</h4>
-                <p>{price}</p>
+                <h5>{price} BGN</h5>
+
+                <Link to={pathToUrl(Path.CarDetails, { carId: _id })} className="details">
+                    <FontAwesomeIcon icon={faChevronRight} />
+                </Link>
             </div>
-            {/* <Link to={pathToUrl(Path.GameDetails, { gameId: _id })} className="details-button">Details</Link> */}
+
         </div>
     );
 };
